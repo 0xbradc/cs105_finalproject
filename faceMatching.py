b85d0b6660dc74161a09f5db7609f86e6c6de6d1
Brad C <48145615+0xbradc@users.noreply.github.com>
2023-12-06 17:55:25 -0500

    Add files via upload

--- faceMatching.py
+++ faceMatching.py
@@ -5,7 +5,7 @@ import urllib.parse
 current_dir = os.path.dirname(os.path.abspath(__file__))
 student_pics_dir = os.path.join(current_dir, "picture_database")
 instagram_pics_dir = os.path.join(current_dir, "picture_samples")
-models = [
+MODELS = [
   "VGG-Face", 
   "Facenet", 
   "Facenet512", 
@@ -16,33 +16,57 @@ models = [
   "Dlib", 
   "SFace",
 ]
-CURRENT_MODEL = models[0]
+CURRENT_MODEL = MODELS[0] # First model by default
 
-for filename in os.listdir(instagram_pics_dir):
-    if os.path.isfile(os.path.join(instagram_pics_dir, filename)):
-        try:
-            match = DeepFace.find(
-                img_path = os.path.join(instagram_pics_dir, filename),
-                db_path = student_pics_dir,
-                model_name=CURRENT_MODEL,
-                enforce_detection = False,
-                silent=True
-            )
-        except KeyboardInterrupt:
-            print("Ctrl+C pressed. Exiting...")
-            exit()
-        except:
-            continue
+def getUserInput():
+    global CURRENT_MODEL, MODELS
+    user_input = input("Enter a number: ")
+    try:
+        number = int(user_input)
+        if 0 <= number <= len(MODELS):
+            CURRENT_MODEL = MODELS[number]
+        else:
+            print(f"The number is not between 0 and {len(MODELS)}. Proceeding with the default model...")
+    except ValueError:
+        print("Invalid input. Proceeding with the default model...")
 
-        # Print top 5 matches
-        print("\nInstagram Picture: ", "\"file://" + urllib.parse.quote(os.path.join(instagram_pics_dir, filename)) + "\"")
-        for i in range(5):
-            print("Match #" + str(i) + ": ", "\"file://" + urllib.parse.quote(match[0].iloc[i]["identity"]) + "\"")
-            print("\tWith Cosine: " + match[0].iloc[i][CURRENT_MODEL + "_cosine"].astype(str))
-        print("\n")
+def runMatching():
+    for filename in os.listdir(instagram_pics_dir):
+        if os.path.isfile(os.path.join(instagram_pics_dir, filename)):
+            try:
+                match = DeepFace.find(
+                    img_path = os.path.join(instagram_pics_dir, filename),
+                    db_path = student_pics_dir,
+                    model_name=CURRENT_MODEL,
+                    enforce_detection = False,
+                    silent=False
+                )
+            except KeyboardInterrupt:
+                print("Ctrl+C pressed. Exiting...")
+                exit()
+            except:
+                continue
 
-        # Print just the top match
-        # print("\nInstagram Picture: ", "\"file://" + urllib.parse.quote(os.path.join(instagram_pics_dir, filename)) + "\"")
-        # print("Closest Match: ", "\"file://" + urllib.parse.quote(match[0].iloc[0]["identity"]) + "\"")
-        # print("\tWith Cosine: " + match[0].iloc[0][CURRENT_MODEL + "_cosine"].astype(str))
-        # print("\n")
+            # Print top 5 matches
+            print("\nInstagram Picture: ", "\"file://" + urllib.parse.quote(os.path.join(instagram_pics_dir, filename)) + "\"")
+            for i in range(5):
+                try:
+                    print("Match #" + str(i) + ": ", "\"file://" + urllib.parse.quote(match[0].iloc[i]["identity"]) + "\"")
+                    print("\tWith Cosine: " + match[0].iloc[i][CURRENT_MODEL + "_cosine"].astype(str))
+                except:
+                    continue
+            print("\n")
+
+            # Print just the top match
+            # print("\nInstagram Picture: ", "\"file://" + urllib.parse.quote(os.path.join(instagram_pics_dir, filename)) + "\"")
+            # try:
+            #     print("Closest Match: ", "\"file://" + urllib.parse.quote(match[0].iloc[0]["identity"]) + "\"")
+            #     print("\tWith Cosine: " + match[0].iloc[0][CURRENT_MODEL + "_cosine"].astype(str))
+            # except:
+            #     continue
+            # print("\n")
+
+
+if __name__ == "__main__":
+    getUserInput()
+    runMatching()
